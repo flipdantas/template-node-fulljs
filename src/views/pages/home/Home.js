88c8d12e516248f3ea99a20336449fbf02a1edb8
Filipe Dantas <filipedantas90@hotmail.com>
2021-05-14 19:13:44 -0300

    first commit

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,17 +1,39 @@
+import api from '../../../service/api';
+
+const getJokesRandom = async () => {
+  const response = await api.get('random')
+  return response.data
+}
+
+const getJokesCategory = async () => {
+  const response = await api.get('categories')
+  return response.data
+}
+
 let Home = {
-  is_private: false,
+  is_private: false,  
 
   render: async () => {
+    const jokes = await getJokesRandom();
+    const categories = await getJokesCategory();
+    console.log(jokes)
+
       let view = `
           <div>
-            <h1>Home</div>
+            <ul class="menu-list">
+            ${categories.map( (category,index) => (
+              `<li class="menu-item" key=${index}>${category}</li>`
+            )).join('')}
+            </ul>
+            <img src=${jokes.icon_url}>
+            <h1>${jokes.value}</h1>
           </div>
       `;
 
       return view
   },
 
-  after_render: async () => {}
+  after_render: async () => {}    
 }
 
 export default Home;
